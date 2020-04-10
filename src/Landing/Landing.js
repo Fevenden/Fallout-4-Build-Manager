ef4d977b457afb98345ad549b263d96dc9cf9cba
fte96 <ftevenden1@gmail.com>
2020-04-09 16:56:46 -0800

    added a small description of the app

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -6,6 +6,7 @@ export default function Landing() {
   return (
     <section id='landing-box' className='box'>
       <h2>Welcome To BuildTech!</h2>
+      <p>A Fallout 4 character build manager</p>
       <p>A better future, Online.</p>
       <Link to='/create-account'>Create an account</Link>
       <Link to='/login'>Login to an existing account</Link>
